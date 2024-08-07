901921d18a16ab8e304b91c971e5307acdc8f14f
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-07 16:34:16 -0300

    fix: Player backward movement

--- Engine/Player.h
+++ Engine/Player.h
@@ -7,9 +7,9 @@
 class Player {
 public:
 	Player(Vec2 position)
-	:
-		position(position)
-	{}
+		:
+		position(position) {
+	}
 
 	Vec2 GetPosition() const {
 		return position;
@@ -20,28 +20,32 @@ public:
 	}
 
 	void ProcessInput(Keyboard& kbd, float deltaTime) {
-		if (kbd.KeyIsPressed('A')) {
-			angle -= rotationSpeed * deltaTime;
-		}
-		if (kbd.KeyIsPressed('D')) {
-			angle += rotationSpeed * deltaTime;
-		}
-		if (kbd.KeyIsPressed('W')) {
-			position += direction * walkSpeed * deltaTime;
-		}
-		if (kbd.KeyIsPressed('S')) {
-			position -= direction * walkSpeed * deltaTime;
-		}
+		float rotationDirection = 0.0f;
+		float walkDirection = 0.0f;
+
+		if (kbd.KeyIsPressed('A')) { rotationDirection -= 1; }
+		if (kbd.KeyIsPressed('D')) { rotationDirection += 1; }
+
+		angle += rotationDirection * rotationSpeed * deltaTime;
 		direction = { cos(angle), sin(angle) };
+
+		if (kbd.KeyIsPressed('W')) {
+			walkDirection += 1;
+		} else if (kbd.KeyIsPressed('S')) {
+			walkDirection -= 1;
+			rotationDirection = -rotationDirection;
+		}
+
+		position += direction * walkSpeed * walkDirection * deltaTime;
 	}
 	void Draw(Graphics& gfx, Color color) const {
 		gfx.DrawCircle((Vei2)position, 5, color);
-		gfx.DrawLineClamped(position, position + direction * 50, color);
+		gfx.DrawLineClamped(position, position + direction * 250, color);
 	}
 private:
 	Vec2 position;
 	Vec2 direction = { 1.0f, 0.0f };
-	float angle = -3.14f/2;
+	float angle = -3.14f / 2;
 	static constexpr float rotationSpeed = 2.0f;
 	static constexpr float walkSpeed = 100.0f;
 };
